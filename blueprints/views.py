83101a66675c6ddf54edc0c701bb89e9e034f77f
Edgar Rafael Jr. <202210027@fit.edu.ph>
2025-06-09 16:17:10 +0800

    modified implementation for reviews

--- blueprints/views.py
+++ blueprints/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
-from .scrape import scrape_site
+from .scrape import scrape_img
+from .scrape import scrape_rev
 
 views_bp = Blueprint('views', __name__) #initialize blueprint
 
@@ -13,11 +14,16 @@ def scrape():
     url = request.form.get('product_url')
     if not url or ("www.lazada.com" not in url and "s.lazada.com.ph" not in url):
         error_msg = 'Enter a valid Lazada link.'
-        return render_template('result.html', scraped_text=error_msg)
-    scraped = scrape_site(url)
+        return render_template('result.html', error_msg=error_msg)
+    scraped_img = scrape_img(url)
+    
+    scraped_rev = scrape_rev(url)  
+    
+    # scraped_rev
+
     # If scrape_site returns product_detail_b64 and gallery_b64, render both
-    if isinstance(scraped, dict) and ('product_detail_b64' in scraped or 'gallery_b64' in scraped):
-        return render_template('result.html', product_detail_b64=scraped.get('product_detail_b64'), gallery_b64=scraped.get('gallery_b64'), product_url=url)
+    if isinstance(scraped_img, dict) and ('product_detail_b64' in scraped_img or 'gallery_b64' in scraped_img):
+        return render_template('result.html', product_detail_b64=scraped_img.get('product_detail_b64'), gallery_b64=scraped_img.get('gallery_b64'), product_url=url, scraped_rev=scraped_rev)
     else:
         error_msg = 'Failed to scrape the product details. Please check the URL or try again later.'
-        return render_template('result.html', scraped_text=error_msg)
+        return render_template('result.html', error_msg=error_msg, product_url=url, scraped_rev=scraped_rev)
